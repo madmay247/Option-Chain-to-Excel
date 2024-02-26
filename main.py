549a321b0ced9eb038d47d1a27caf9a3b96d839f
Madmay <mrai748@gmail.com>
2024-02-26 14:23:29 +0530

    Updated

--- main.py
+++ main.py
@@ -90,21 +90,11 @@ def fetch_option_chain(symbol, xl=False):
         return None
     
     url = f'https://www.nseindia.com/api/option-chain-indices?symbol={symbol}'
-<<<<<<< HEAD
-    
-    headers = { 'Connection': 'keep-alive',
-               'Cache-Control': 'max-age=0',
-               'DNT': '1', 'Upgrade-Insecure-Requests': '1',
-               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36',
-               'Sec-Fetch-User': '?1',
-               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-               'Sec-Fetch-Site': 'none',
-               'Sec-Fetch-Mode': 'navigate',
-               'Accept-Encoding': 'gzip, deflate, br',
-               'Accept-Language': 'en-US,en;q=0.9,hi;q=0.8'}
-=======
-    headers = {'User-Agent': 'Mozilla/5.0'}
->>>>>>> a33b3a0e5928cf7834ff48618cc44bee76552342
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
+                        'like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+        'accept-language': 'en,gu;q=0.9,hi;q=0.8',
+        'accept-encoding': 'gzip, deflate, br'}
     
     # Retry mechanism for request
     max_retries = 3
